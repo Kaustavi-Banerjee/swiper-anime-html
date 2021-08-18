058a1970269321cc0e10b3c6a67c007203b2042a
Prosenjit Manna <prosenjit@itobuz.com>
2021-08-18 19:23:26 +0530

    slider animation logic added

--- js/main.js
+++ js/main.js
@@ -1,49 +1,38 @@
+function bannerAnimation(swiper) {
+  var slide = swiper.slides[swiper.activeIndex];
+
+  var t1 = anime.timeline({
+    easing: 'easeOutExpo',
+    duration: 800
+  })
+  t1.add({
+    targets: slide.querySelector('.sub-heading'),
+    opacity: 1,
+    translateY: -20,
+  })
+  .add({
+    targets: slide.querySelector('.main-title'),
+    opacity: 1,
+    translateX: 20,
+  },'1000')
+  .add({
+    targets: slide.querySelector('.btn'),
+    opacity: 1,
+    translateY: -20,
+  }, '1400')
+}
+
 const swiper = new Swiper('.swiper-container', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
   on: {
-    init: () => {
-      var t1 = anime.timeline({
-        easing: 'easeOutExpo',
-        duration: 800
-      })
-      t1.add({
-        targets: '.sub-heading',
-        opacity: 1,
-        translateY: -20,
-      })
-      .add({
-        targets: '.main-title',
-        opacity: 1,
-        translateX: 20,
-      },'1000')
-      .add({
-        targets: '.btn',
-        opacity: 1,
-        translateY: -20,
-      }, '1400')
+    activeIndexChange: (swiper) => {
+      bannerAnimation(swiper);
+    },
+    afterInit: (swiper) => {
+      bannerAnimation(swiper);
     }
   }
 })
-
-// swiper.on('init', function () {
-//   anime({
-//     targets: '.sub-heading',
-//     translateY: -20,
-//     opacity: 1,
-//     easing: 'easeInOutQuad',
-//     duration: 800
-//   });
-// });
-
-// swiper.on('slideChange', function () {
-//   anime({
-//     targets: '.sub-heading',
-//     translateY: -20,
-//     opacity: 1,
-//     easing: 'easeInOutQuad',
-//     duration: 800
-//   });
-// });
